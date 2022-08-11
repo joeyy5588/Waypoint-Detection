9546bb7c625d92b5bf05b95acb16cc57863c689b
joeyy5588 <joeyy5588@gmail.com>
2022-08-12 01:59:19 +0800

    dataset

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import torch
-from data import Panorama_Dataset
+from data.dataset import Panorama_Dataset
 
 train_data_dir = '/datadrive_c/chengfu/waypoint/data/panorama'
 train_dataset = Panorama_Dataset(train_data_dir)
