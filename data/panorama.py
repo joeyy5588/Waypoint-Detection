65e580b9058e806c369fac63209acd0b18662602
joeyy5588 <joeyy5588@gmail.com>
2022-08-12 01:15:12 +0800

    first commit

---
+++ data/panorama.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+
+class Panorama_Dataset:
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
