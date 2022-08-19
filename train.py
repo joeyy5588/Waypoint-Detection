e52cbe720fffcdf966ceb89a5aae2d218a970df2
Joey Yang <joeyyang@JoeydeMacBook-Air.local>
2022-08-20 01:16:20 +0800

    update configuration

--- train.py
+++ train.py
@@ -6,9 +6,9 @@ from trainer.trainer import WaypointTrainer
 from transformers import TrainingArguments, AutoConfig
 from torch.utils.data import DataLoader
 
-train_data_dir = '/mnt/alfworld/data/panorama_valid_seen'
-eval_data_dir = '/mnt/alfworld/data/panorama_valid_seen'
-predict_xyz = True
+train_data_dir = '/datadrive_c/chengfu/waypoint/data/panorama'
+eval_data_dir = '/datadrive_c/chengfu/waypoint/data/panorama_valid_seen'
+predict_xyz = False
 train_dataset = Panorama_Dataset(train_data_dir, predict_xyz=predict_xyz)
 eval_dataset = Panorama_Dataset(eval_data_dir, predict_xyz=predict_xyz)
 tokenizer = train_dataset.tokenizer
@@ -18,13 +18,13 @@ config = AutoConfig.from_pretrained('bert-base-uncased')
 model = Waypoint_Transformer(config, predict_xyz=predict_xyz).from_pretrained('bert-base-uncased')
 
 training_args = TrainingArguments(
-    output_dir="./output",
+    output_dir="/datadrive_c/chengfu/waypoint/output/predict_rt",
     evaluation_strategy="epoch",
     learning_rate=5e-5,
     warmup_ratio=0.1,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    num_train_epochs=3,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=20,
     weight_decay=0.0001,
     logging_steps=20,
     report_to='tensorboard'
