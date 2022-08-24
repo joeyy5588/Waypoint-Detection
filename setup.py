1a4217b99af674e42c86c5282340b695947afa52
Joey Yang <joeyyang@JoeydeMacBook-Air.local>
2022-08-24 15:40:12 +0800

    add ai2thor compatibility

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     description="Waypoint Navigation for ALFRED",
+    include_package_data=True,
     url='https://github.com/joeyy5588/Waypoint-Detection'
 )
    
